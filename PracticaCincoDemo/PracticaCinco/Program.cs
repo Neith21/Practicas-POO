7cb9e24a24865d6ca882f80374b3e9dc37400930
Neith21 <cristian.1945theend@gmail.com>
2024-03-09 16:24:29 -0600

    Subiendo práctica 5

---
+++ PracticaCincoDemo/PracticaCinco/Program.cs
@@ -0,0 +1,43 @@
+﻿public class PersonaClase
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+
+}
+
+public struct PersonaStruct
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PersonaClase persona1 = new PersonaClase();
+        persona1.Nombre = "Juan";
+        persona1.Edad = 5;
+
+        PersonaClase persona2 = new PersonaClase();
+        persona2 = persona1; //Se hace una referencia
+
+        persona2.Nombre = "Pedro";
+
+        Console.WriteLine($"Persona 1: Nombre = {persona1.Nombre}, Edad = {persona1.Edad}");
+        Console.WriteLine($"Persona 2: Nombre = {persona2.Nombre}, Edad = {persona2.Edad}");
+
+
+        //
+        PersonaStruct personaStruct1 = new PersonaStruct();
+        personaStruct1.Nombre = "Juan";
+        personaStruct1.Edad = 5;
+
+        PersonaStruct PersonaStruct2 = personaStruct1; //Se hace una copia
+        PersonaStruct2.Nombre = "Maria"; 
+
+
+        Console.WriteLine($"Persona 1: Nombre = {personaStruct1.Nombre}, Edad = {personaStruct1.Edad}");
+        Console.WriteLine($"Persona 2: Nombre = {PersonaStruct2.Nombre}, Edad = {PersonaStruct2.Edad}");
+    }
+}
---
+++ PracticaCincoDemo/PracticaV/Program.cs
@@ -0,0 +1,71 @@
+﻿public interface IGestorLibros
+{
+    void MostrarLibro();
+}
+
+public struct Libro : IGestorLibros
+{
+    public string Titulo;
+    public string Autor;
+    public int AnioPublicacion;
+
+    public Libro(string titulo, string autor, int anioPublicacion)
+    {
+        Titulo = titulo;
+        Autor = autor;
+        AnioPublicacion = anioPublicacion;
+    }
+
+    public void MostrarLibro()
+    {
+        Console.WriteLine($"Titulo {Titulo}, Autor {Autor}, Año de publicación: {AnioPublicacion}");
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Libro> libro = new List<Libro>();
+
+        while (true)
+        {
+            Console.WriteLine("1. Agregar libro.");
+            Console.WriteLine("2. Mostrar libro");
+            Console.WriteLine("3. Salir");
+            Console.Write("Ingrese una opción: ");
+            string opcion = Console.ReadLine();
+
+            switch ( opcion )
+            {
+                case "1":
+                    Console.Write("Ingrese el titulo del libro: ");
+                    string titulo = Console.ReadLine();
+                    Console.Write("Ingrese el autor del libro: ");
+                    string autor = Console.ReadLine();
+                    Console.Write("Ingrese el año de publicación del libro: ");
+                    int anioPublicacion;
+
+                    while(!int.TryParse(Console.ReadLine(), out anioPublicacion))
+                    {
+                        Console.Write("Año invalido, intente de nuevo: ");
+                    }
+                    libro.Add(new Libro(titulo, autor, anioPublicacion));
+                    break;
+                case "2":
+                    Console.WriteLine("Lista de libros");
+                    foreach (var li in libro)
+                    {
+                        li.MostrarLibro();
+                    }
+                    break;
+                case "3":
+                    Console.WriteLine("Saliendo del programa...");
+                    return;
+                default:
+                    Console.WriteLine("Opción inválida. Intente de nuevo.");
+                    break;
+            }
+        }
+    }
+}
