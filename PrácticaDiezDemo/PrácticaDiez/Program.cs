5a3bd14390b793e402153d3655ebe326553909d9
Neith21 <cristian.1945theend@gmail.com>
2024-04-19 15:03:26 -0600

    Subiendo práctica 10

---
+++ PrácticaDiezDemo/PrácticaDiez/Program.cs
@@ -0,0 +1,67 @@
+﻿using Spectre.Console;
+
+record Person(string Name, int Age);
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string csvFilePath = PromptFilePath();
+
+        DataRetriver dataRetriver = new DataRetriver(csvFilePath);
+
+        List<Person> people = dataRetriver.LoadPeople();
+
+        var adults = people.Where(p => p.Age >= 18);
+
+        RenderResults(adults);
+    }
+
+    static string PromptFilePath()
+    {
+        AnsiConsole.MarkupLine("[yellow]Ingrese la ruta del archivo CVS:[/]");
+        return AnsiConsole.Ask<string>("Ruta del archivo:");
+    }
+
+    static void RenderResults(IEnumerable<Person> people)
+    {
+        var table = new Table();
+
+        table.AddColumn("Nombre");
+        table.AddColumn("Edad");
+
+        foreach (var person in people)
+        {
+            table.AddRow(person.Name, person.Age.ToString());
+        }
+
+        AnsiConsole.Render(table);
+    }
+}
+
+class DataRetriver
+{
+    private string _csvFilePath;
+
+    public DataRetriver(string csvFilePath)
+    {
+        _csvFilePath = csvFilePath;
+    }
+
+    public List<Person> LoadPeople()
+    {
+        List<Person> people = new List<Person>();
+
+        string[] lines = File.ReadAllLines(_csvFilePath);
+
+        foreach (var line in lines)
+        {
+            string[] parts = line.Split(',');
+
+            Person person = new Person(parts[0], Convert.ToInt32(parts[1]));
+            people.Add(person);
+        }
+
+        return people;
+    }
+}
